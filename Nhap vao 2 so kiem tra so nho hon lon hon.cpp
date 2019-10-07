737a2bbfaef008c00141e8901f5f5b525f8c9447
Ngoclam0703 <phamngoclam2003@gmail.com>
2019-10-07 23:07:37 +0700

    Btap

---
+++ Nhap vao 2 so kiem tra so nho hon lon hon.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+	int main (){
+		int a,b;
+		printf("nhap vao 2 so can kiem tra ");
+		scanf("%d %d",&a,&b);
+		if (a>b){
+	    printf("so nho hon la %d\n",b);
+		}
+	    if (a<b){
+	    printf("so lon hon la %d\n",b);	
+		}
+		return 0;
+	}
+	
